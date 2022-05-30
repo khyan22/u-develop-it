6c7bf4d3c2406f642997426ed93760114a21f517
khyan22 <khyanalvarez@gmail.com>
2022-05-30 18:53:18 -0400

    left joined parties into candidates to display each candidates party name | also updated get routes

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ const db = mysql.createConnection(
 
 //shows all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+    AS party_name
+    FROM  candidates
+    LEFT JOIN parties
+    ON candidates.party_id = parties.id`;
     db.query(sql, (err, rows) => {
         if (err) {
             res.statusMessage(400).json({ error: err.message });
@@ -37,7 +41,12 @@ app.get('/api/candidates', (req, res) => {
 
 // shows A single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                 AS party_name
+                 FROM  candidates
+                 LEFT JOIN parties
+                 ON candidates.party_id = parties.id
+                 WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
